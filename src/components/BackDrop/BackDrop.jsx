5f31e1e4a7ec6635e2f0a2c9309a1b3ba7de06f5
DanteThuta <dantethuta97@gmail.com>
2022-06-01 18:30:56 +0630

    Creating About Section(In Progress)

--- src/components/BackDrop/BackDrop.jsx
+++ src/components/BackDrop/BackDrop.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import MyImage from "../../assets/me.png";
+import "./BackDrop.css";
 
 const BackDrop = () => {
   return (
     <section>
-      <div className="container">
+      <h5>About me</h5>
+      <h2>React Developer</h2>
+      <div className="container backdrop__container">
         <div className="image__section">
           <img src={MyImage} alt="" />
         </div>
@@ -15,7 +18,7 @@ const BackDrop = () => {
             Lorem ipsum dolor sit, amet consectetur adipisicing elit.
             Laudantium, placeat!
           </p>
-          <button>View</button>
+          <button className="btn">View</button>
         </div>
       </div>
     </section>
