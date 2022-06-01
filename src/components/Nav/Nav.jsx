0bf4885edfeb61702032a3ac2306e742f214895a
DanteThuta <dantethuta97@gmail.com>
2022-06-01 18:01:31 +0630

    NavBar Section Finished

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,29 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.css";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Nav = () => {
+  const [navToggle, setNavToggle] = useState(false);
+
+  const handleToggle = () => {
+    setNavToggle((prev) => !prev);
+  };
   return (
     <nav>
       <div className="nav__header">KoThu</div>
-      <ul className="nav__group">
+      <ul className={navToggle === true ? "nav__group active" : "nav__group"}>
         <li className="nav__item">
           <a href="#" className="nav__link">
             Home
           </a>
         </li>
         <li className="nav__item">
-          <a href="#" className="nav__link">Skill</a>
+          <a href="#" className="nav__link">
+            Skill
+          </a>
         </li>
         <li className="nav__item">
-          <a href="#" className="nav__link">Home</a>
+          <a href="#" className="nav__link">
+            Portfolio
+          </a>
         </li>
         <li className="nav__item">
-          <a href="#" className="nav__link">Home</a>
+          <a href="#" className="nav__link">
+            Experience
+          </a>
         </li>
         <li className="nav__item">
-          <a href="#" className="nav__link">Home</a>
+          <a href="#" className="nav__link">
+            Contact
+          </a>
         </li>
       </ul>
+      <div className="nav__menu" onClick={handleToggle}>
+        <AiOutlineMenu />
+      </div>
     </nav>
   );
 };
