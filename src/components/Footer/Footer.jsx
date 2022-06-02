de290701d9e37079fa3522be86700b5ee86acee6
DanteThuta <dantethuta97@gmail.com>
2022-06-02 18:13:39 +0630

    Creating Footer Section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import MyImage from "../../assets/ninja.png";
 
+import {
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaInstagramSquare,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import "./Footer.css";
+
 const Footer = () => {
   return (
-    <section>
-      <div className="container">
+    <footer>
+      <div className="container footer__container">
         <div className="logo">
-          <img src={MyImage} alt="" />
+          <h2>KoThu Portfolio</h2>
+          {/* <img src={MyImage} alt="" /> */}
         </div>
         <div className="website__links">
+          <h2>Information</h2>
           <ul>
             <li>Test</li>
             <li>Test</li>
@@ -16,12 +27,32 @@ const Footer = () => {
             <li>Test</li>
           </ul>
         </div>
-        <div className="copyright__content">
-          <h2>KoThu Portfolio</h2>
-          <p>&copy; All rights Reserved.</p>
+        <div className="social__links">
+          <div className="social__links-items">
+            <a href="">
+              <FaFacebookSquare />
+            </a>
+          </div>
+          <div className="social__links-items">
+            <a href="">
+              <FaInstagramSquare />
+            </a>
+          </div>
+          <div className="social__links-items">
+            <FaTwitterSquare />
+          </div>
+          <div className="social__links-items">
+            <a href="">
+              <FaLinkedin />
+            </a>
+          </div>
         </div>
       </div>
-    </section>
+      <div className="copyright__content">
+        <h2>KoThu Portfolio</h2>
+        <p>&copy; All rights Reserved.</p>
+      </div>
+    </footer>
   );
 };
 
