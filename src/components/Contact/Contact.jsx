415214ca4285fa8bac3ce39fd02e7f7fbae0e57a
DanteThuta <dantethuta97@gmail.com>
2022-06-02 17:41:33 +0630

    Contact Me Section Finished

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,23 +1,47 @@
 import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
     <section>
-      <div className="container">
+      <div className="container contact__container">
         {/* Form Section */}
-        <div className="form">
-          <form action="">
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
+        <div className="contact__form">
+          <h2>Email me from Here:</h2>
+          <form action="" className="contact__formlist">
+            <input type="text" placeholder="Enter your Name" />
+            <input type="email" placeholder="Enter your Email" />
+            <textarea
+              name=""
+              id=""
+              cols="20"
+              rows="5"
+              placeholder="Enter your Thoughts about me"
+            ></textarea>
 
-            <button>Submit</button>
+            <button className="btn">Submit</button>
           </form>
         </div>
         {/* Contact Shortcut Section */}
         <div className="shortcut__contact">
-          <article>
+          <article className="contact__item">
             <h5>Lorem ipsum dolor sit.</h5>
+            <h2>Facebook</h2>
+            <button className="btn contact__btn">Message Me</button>
+
+            <a href=""></a>
+          </article>
+          <article className="contact__item">
+            <h5>Lorem ipsum dolor sit.</h5>
+            <h2>Facebook</h2>
+            <button className="btn contact__btn">Message Me</button>
+
+            <a href=""></a>
+          </article>
+          <article className="contact__item">
+            <h5>Lorem ipsum dolor sit.</h5>
+            <h2>Facebook</h2>
+            <button className="btn contact__btn">Message Me</button>
 
             <a href=""></a>
           </article>
